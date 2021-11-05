d6fcfb749cb7ce03f836191f049a5f6594b92668
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-11-05 00:07:05 -0300

    Initial implementation
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- cursor.go
+++ cursor.go
@@ -1,1 +1,119 @@
 package cursor
+
+import (
+	"bytes"
+	"errors"
+	"io"
+)
+
+var _ io.ReadWriteSeeker = (*Cursor)(nil)
+
+type Cursor struct {
+	buf []byte
+	pos int
+}
+
+func New(buf []byte) *Cursor {
+	return &Cursor{
+		buf: buf,
+		pos: 0,
+	}
+}
+
+func Read(r io.Reader) (*Cursor, error) {
+	buf, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return New(buf), nil
+}
+
+func (c *Cursor) Bytes() []byte {
+	return c.buf
+}
+
+func (c *Cursor) Position() int {
+	return c.pos
+}
+
+func (c *Cursor) SetPosition(pos int) {
+	c.pos = pos
+}
+
+func (c *Cursor) Remaining() []byte {
+	len := min(c.pos, len(c.buf))
+	return c.buf[len:]
+}
+
+func (c *Cursor) IsEmpty() bool {
+	return c.pos >= len(c.buf)
+}
+
+func (c *Cursor) Clone() *Cursor {
+	buf := make([]byte, len(c.buf))
+	copy(buf, c.buf)
+
+	return &Cursor{
+		buf: buf,
+		pos: c.pos,
+	}
+}
+
+func (c *Cursor) CloneFrom(other *Cursor) {
+	c.buf = make([]byte, len(other.buf))
+	copy(c.buf, other.buf)
+	c.pos = other.pos
+}
+
+func (c *Cursor) Read(p []byte) (n int, err error) {
+	r := bytes.NewReader(c.Remaining())
+	if n, err := r.Read(p); err != nil {
+		return n, err
+	}
+
+	c.pos += n
+	return n, nil
+}
+
+func (c *Cursor) Write(p []byte) (n int, err error) {
+	if len(c.buf) <= len(p) {
+		buf := make([]byte, len(c.buf)+len(p))
+		copy(buf, c.buf)
+		c.buf = buf
+	}
+
+	pos := min(c.pos, len(c.buf))
+	count := copy(c.buf[pos:], p)
+	c.pos += count
+	return count, nil
+}
+
+func (c *Cursor) Seek(offset int64, whence int) (int64, error) {
+	var basePos int
+	switch whence {
+	case io.SeekStart:
+		c.pos = int(offset)
+		return offset, nil
+	case io.SeekEnd:
+		basePos = len(c.buf)
+	case io.SeekCurrent:
+		basePos = c.pos
+	}
+
+	newPos := basePos + int(offset)
+	if newPos <= 0 {
+		return -1, errors.New("invalid seek to negative position")
+	}
+
+	c.pos = newPos
+	return int64(newPos), nil
+}
+
+func min(l, r int) int {
+	if l < r {
+		return l
+	}
+
+	return r
+}
---
+++ cursor_test.go
@@ -0,0 +1,148 @@
+package cursor_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/fdschonborn/cursor"
+)
+
+func Example() {
+	writeTenBytesAtEnd := func(ws io.WriteSeeker) error {
+		if _, err := ws.Seek(-10, io.SeekEnd); err != nil {
+			return err
+		}
+
+		for i := byte(0); i < 10; i++ {
+			if _, err := ws.Write([]byte{i}); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	buf := cursor.New(make([]byte, 15))
+	if err := writeTenBytesAtEnd(buf); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(buf.Bytes()[5:15])
+	// Output:
+	// [0 1 2 3 4 5 6 7 8 9]
+}
+
+func ExampleCursor_Position() {
+	buf := cursor.New([]byte{1, 2, 3, 4, 5})
+	fmt.Println(buf.Position())
+
+	buf.Seek(2, io.SeekCurrent)
+	fmt.Println(buf.Position())
+
+	buf.Seek(-1, io.SeekCurrent)
+	fmt.Println(buf.Position())
+
+	// Output:
+	// 0
+	// 2
+	// 1
+}
+
+func ExampleCursor_SetPosition() {
+	buf := cursor.New([]byte{1, 2, 3, 4, 5})
+	fmt.Println(buf.Position())
+
+	buf.SetPosition(2)
+	fmt.Println(buf.Position())
+
+	buf.SetPosition(4)
+	fmt.Println(buf.Position())
+
+	// Output:
+	// 0
+	// 2
+	// 4
+}
+
+func ExampleCursor_Remaining() {
+	buf := cursor.New([]byte{1, 2, 3, 4, 5})
+	fmt.Println(buf.Remaining())
+
+	buf.SetPosition(2)
+	fmt.Println(buf.Remaining())
+
+	buf.SetPosition(4)
+	fmt.Println(buf.Remaining())
+
+	buf.SetPosition(6)
+	fmt.Println(buf.Remaining())
+
+	// Output:
+	// [1 2 3 4 5]
+	// [3 4 5]
+	// [5]
+	// []
+}
+
+func ExampleCursor_IsEmpty() {
+	buf := cursor.New([]byte{1, 2, 3, 4, 5})
+
+	buf.SetPosition(2)
+	fmt.Println(!buf.IsEmpty())
+
+	buf.SetPosition(5)
+	fmt.Println(buf.IsEmpty())
+
+	buf.SetPosition(10)
+	fmt.Println(buf.IsEmpty())
+
+	// Output:
+	// true
+	// true
+	// true
+}
+
+func TestWriter(t *testing.T) {
+	w := cursor.New([]byte{})
+	var (
+		n   int
+		err error
+	)
+
+	n, err = w.Write([]byte{0})
+	if err != nil {
+		t.Fatal(n)
+	}
+
+	if n != 1 {
+		t.Errorf("Expected n to be 1, got %d", n)
+	}
+
+	n, err = w.Write([]byte{1, 2, 3})
+	if err != nil {
+		t.Fatal(n)
+	}
+
+	if n != 3 {
+		t.Errorf("Expected n to be 3, got %d", n)
+	}
+
+	n, err = w.Write([]byte{4, 5, 6, 7})
+	if err != nil {
+		t.Fatal(n)
+	}
+
+	if n != 4 {
+		t.Errorf("Expected n to be 4, got %d", n)
+	}
+
+	expected := []byte{0, 1, 2, 3, 4, 5, 6, 7}
+	actual := w.Bytes()
+
+	result := bytes.Compare(expected, actual)
+	if result != 0 {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
